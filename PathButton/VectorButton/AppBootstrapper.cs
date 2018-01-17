3dd5f024e215e9c78e02198ebbf49d1f967d15bf
SOFTEQ\agridiushko <alexey.gridiushko@softeq.com>
2018-01-17 17:11:10 +0300

    no

---
+++ PathButton/VectorButton/AppBootstrapper.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Windows;
+using Caliburn.Micro;
+using VectorButton.ViewModel;
+
+namespace VectorButton
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            var config = new TypeMappingConfiguration
+            {
+                DefaultSubNamespaceForViewModels = "ViewModels",
+                DefaultSubNamespaceForViews = "Views"
+            };
+            ViewModelLocator.ConfigureTypeMappings(config);
+            ViewLocator.ConfigureTypeMappings(config);
+
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainWindowViewModel>();
+        }
+    }
+}
---
+++ PathButton/VectorButton/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+
+namespace VectorButton.ViewModel
+{
+    public class MainWindowViewModel : PropertyChangedBase
+    {
+        private string _newText;
+
+        public MainWindowViewModel()
+        {
+            NewText = "uqeiruqw";
+        }
+
+        public string NewText
+        {
+            get { return _newText; }
+            set
+            {
+                _newText = value;
+                NotifyOfPropertyChange(() => NewText);
+            }
+        }
+    }
+}
